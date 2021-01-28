e436383f5de1a1dd635eafef0e9d1dd04e1297ae
Seamus O'Sullivan <info@seamuso.com>
2021-01-28 11:34:19 +0100

    first commit

---
+++ labnames.js
@@ -0,0 +1,7 @@
+ 
+let labNames = [
+    {text: "", clicked: false}, 
+    {text: "test1", clicked: false}, 
+    {text: "test2", clicked: false}, 
+    {text: "test3", clicked: false}, 
+    {text: "test4", clicked: false} ];
