69fefe60e85b4478194c61f48a50b4099668b332
Seamus O'Sullivan <info@seamuso.com>
2021-01-28 18:36:14 +0100

    css

---
+++ clock.js
@@ -0,0 +1,50 @@
+// let counter = 0;
+// let timeLeft = 60;
+// function setup() {
+//     let timer = select('#timer');
+//     timer.html('counter')
+    
+    
+//     function timeIt() {
+//        counter++;
+//        timer.html(timeLeft - counter); 
+//     }
+
+//     setInterval(timeIt, 1000);
+
+// }
+
+let timeLeft = 10;
+const gameOver = document.querySelector(".game-over")
+const timeLeftDisplay = document.querySelector('#time-left')
+document.addEventListener('DOMContentLoaded', () => {
+   countDown() 
+        
+    })
+    
+    function countDown() {
+    let countDown = setInterval(function() {
+        if(timeLeft <= 0) {
+            timeLeft = 0
+           clearInterval(countDown) 
+           remove()
+           gameOver.innerHTML = "GAME OVER!"
+           
+           
+
+           
+            
+            
+           
+        }
+        timeLeftDisplay.innerHTML = timeLeft
+        timeLeft -= 1;
+    }, 1000)
+    
+    }
+
+    let clockTime = document.querySelector('#timeLeft')
+
+
+
+ 
--- main.js
+++ main.js
@@ -5,67 +5,47 @@ document.addEventListener('mousemove', e => {
 cursor.setAttribute("style", "top: "+e.pageY+"px; left: "+e.pageX+"px;")
 })
 
+let timer = 5;
 let x;
 let y;
-let radius = 200;
+let radius = 100;
 let elements;
 
 let score = 0;
-let test = 'this is a test';
 
-let htmlScore = document.querySelector('#string')
+let htmlScore = document.querySelector('#score')
+// clockTime.innerHTML = `Time until you are overwhelmed: ${timeLeft}`;
 
-//const labNames = ['test', 'test2', 'test3', 'test4','test5', 'test6', 'test7', 'test8','test9', 'test12', 'test13', 'test14','test15', 'test16', 'test17', 'test18'];
+
 
 
 function preload() {
 }
 
 function setup() {
-createCanvas(2000, 1500)
-
+createCanvas(1000, 1000)
 }
 
-function draw() {  
+function draw() {    
 clear()
+
 textSize(40)
 
-//  text(test, x, y, 2000, 1500)
-// x = [ 1, 2,3,4,5]
-// x.filter(num => {
-//     if (num % 3 === 0) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// })
-
-// newX = [1,3]
 
 labNames = labNames.filter(lab => {
-    x = random(100, 2000);
-    y = random(100, 2000);
+    x = random(100, 900);
+    y = random(100, 900);
     if (lab.clicked) {
         return false;
     } else {
         lab.x = x;
         lab.y = y;
-        text(lab.text, x, y, 100, 2000)
+        text(lab.text, x, y, 100, 900)
         frameRate(1)
         return true;
     }
 
     });
-   
-    // elements = labNames.forEach(lab => {
-    //     let x = random(100, 2000);
-    //     let y = random(100, 2000);
-    //     text(lab, x, y, 100, 20000)
-    //     frameRate(10)
-    //     });
-
-
 
 if(frameCount % 100 === 0) {
     //console.log('frame')
@@ -74,7 +54,7 @@ if(frameCount % 100 === 0) {
    }
  }
 
-
+ 
 
  function mousePressed() {
      labNames.forEach(lab => {
@@ -83,26 +63,21 @@ if(frameCount % 100 === 0) {
              lab.clicked = true;
              score ++;
              htmlScore.innerHTML = `your score is:  ${score}`;
+        
+  
+             
+             
+             
          }
      })
-    // if (int(dist(x,y,mouseX,mouseY) < radius)) {
-    //     // labNames.splice(word)
-    //     // //shownWords.push(labNames)
-    //     // console.log('was clicked')
-    //     // console.log(shownWords)
 
-    //     score ++;
-    // console.log(score)
-
-    // htmlScore.innerHTML = `your score is:  ${score}`;
-    }
+}
     
 
 
     
     
-   
-  
+
 
 
 
